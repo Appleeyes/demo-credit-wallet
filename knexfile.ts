b8c74dd882cec2db7822d916e2c4fffdf4b5efd2
Appleeyes <appleeyespackage@gmail.com>
2024-06-30 17:57:32 +0100

    feat: set up database and knex

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,16 @@
 import { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql2",
     connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "",
-      database: "democredit",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       tableName: "knex_migrations",
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,1 +1,5 @@
-// 
+import { Request, Response } from "express";
+
+class UserController {}
+
+export default UserController;
